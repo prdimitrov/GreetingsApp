3bfa26de5862cc323c307b65bdb38c61957781ed
prdimitrov <prdimitrov97@gmail.com>
2024-11-05 10:42:35 +0200

    - Add TextView, add EditText, add Button to the ConstraintLayout in activity_main.xml - Implement TextView, EditText and Button in the MainActivity.java class - Add background image in drawable folder, for the ConstraintLayout.

--- app/src/main/java/com/example/greetingsapp/MainActivity.java
+++ app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.greetingsapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            textView = findViewById(R.id.myTextView);
+            EditText editText = findViewById(R.id.myEditText);
+            Button btn = findViewById(R.id.myButton);
+
             return insets;
         });
     }
